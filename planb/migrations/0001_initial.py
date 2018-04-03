8782971832a36accea34028703c0a959295c28f2
Walter Doekes <walter+github@wjd.nu>
2018-04-03 16:53:28 +0200

    models: Silence old django complaint about naive datetime

--- planb/migrations/0001_initial.py
+++ planb/migrations/0001_initial.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 
 import datetime
 from django.db import migrations, models
+from django.utils import timezone
 import django.db.models.deletion
 import planb.fields
 
@@ -34,7 +35,7 @@ class Migration(migrations.Migration):
                 ('excludes', planb.fields.FilelistField(blank=True, max_length=1023)),
                 ('running', models.BooleanField(default=False)),
                 ('priority', models.IntegerField(default=0)),
-                ('date_complete', models.DateTimeField(default=datetime.datetime(1970, 1, 2, 0, 0), verbose_name='Complete date')),
+                ('date_complete', models.DateTimeField(default=datetime.datetime(1970, 1, 2, 0, 0, tzinfo=timezone.utc), verbose_name='Complete date')),
                 ('complete_duration', models.PositiveIntegerField(default=0, help_text='Duration in seconds of last successful job.', verbose_name='Time')),
                 ('enabled', models.BooleanField(default=True)),
                 ('queued', models.BooleanField(default=False)),

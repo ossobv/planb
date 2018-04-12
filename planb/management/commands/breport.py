4455e0f913a752f56f779eb4fa741d99bee5c9dd
Walter Doekes <walter+github@wjd.nu>
2018-04-12 09:04:25 +0200

    breport: Refactor command so we can easily fix/change layout

--- planb/management/commands/breport.py
+++ planb/management/commands/breport.py
@@ -1,89 +1,157 @@
 from datetime import timedelta
+from fnmatch import fnmatch
 from subprocess import check_output
 
 from django.conf import settings
 from django.core.mail import get_connection
 from django.core.mail.message import EmailMultiAlternatives
 from django.core.management.base import BaseCommand
-from django.db.models import Q
 from django.template.loader import render_to_string
 
 from django.utils import timezone
 from django.utils.translation import ugettext as _
 
-from planb.models import HostConfig
+from planb.models import HostConfig, HostGroup
 
 
 class Command(BaseCommand):
-    help = 'Email backup report'
+    help = 'Create backup report and optionally e-mail'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--output', choices=('text', 'html', 'email'), default='text',
+            help='What kind of output; option email sends the mail')
+        parser.add_argument('--force', action='store_true', help=(
+            'If output is email and it was sent recently, send anyway'))
+        parser.add_argument('--with-disabled', action='store_true', help=(
+            'Also list disabled (inactive) hosts'))
+        parser.add_argument('groups', nargs='?', default='*', help=(
+            'Which hostgroups to operate on, allows globbing'))
+        parser.add_argument('hosts', nargs='?', default='*', help=(
+            'Which hostconfigs to operate on, allows globbing'))
+
+        return super().add_arguments(parser)
 
     def handle(self, *args, **options):
-        qs = (
-            HostConfig.objects
-            .filter(hostgroup__notify_email__contains='@')
-            .select_related('hostgroup')
-            .order_by('hostgroup__name', 'friendly_name'))
-        self.send_monthly_reports(qs)
+        if options['output'] == 'text':
+            func = self.output_text
+        elif options['output'] == 'html':
+            func = self.output_html
+        elif options['output'] == 'email':
+            func = self.output_email
+        else:
+            assert False, options
 
-    def send_monthly_reports(self, qs):
-        a_while_ago = timezone.now() - timedelta(days=25)
-        qs = qs.filter(
-            Q(hostgroup__last_monthly_report=None) |
-            Q(hostgroup__last_monthly_report__lt=a_while_ago))
+        hostconfigs = self.get_hostconfigs(
+            options['groups'], options['hosts'])
+
+        self.run_per_group(func, hostconfigs, options['force'])
+
+    def get_hostconfigs(self, groups_glob, hosts_glob, with_disabled=False):
+        groups = HostGroup.objects.all()
+        hosts = HostConfig.objects.all()
+
+        groups = [
+            group for group in groups if fnmatch(group.name, groups_glob)]
+        hosts = HostConfig.objects.filter(id__in=(
+            host.id for host in (
+                hosts.filter(hostgroup__in=groups)
+                .prefetch_related('hostgroup'))
+            if fnmatch(host.friendly_name, hosts_glob)))
+
+        return hosts
+
+    def run_per_group(self, func, qs, force_send):
+        # Fix so we can aggregate by group below.
+        qs = qs.order_by(
+            'hostgroup__name', 'hostgroup__id', 'friendly_name', 'id')
 
         lastgroup = None
         hosts = []
         for host in qs:
             if lastgroup != host.hostgroup:
                 if lastgroup is not None:
-                    self.email_hostgroup(lastgroup, hosts)
+                    func(lastgroup, hosts, force_send)
                 lastgroup = host.hostgroup
                 hosts = []
             hosts.append(host)
 
         if lastgroup is not None:
-            self.email_hostgroup(lastgroup, hosts)
+            func(lastgroup, hosts, force_send)
 
-    def email_hostgroup(self, hostgroup, hosts):
+    def generate_subject(self, hostgroup, hosts):
         hosts_disabled = sum(
             1 for i in hosts if not i.enabled)
         hosts_failed = sum(
             1 for i in hosts if i.enabled and not i.last_backuprun.success)
 
-        context = {
-            'hostgroup': hostgroup,
-            'hosts': hosts,
-            'company_name': settings.COMPANY_NAME,
-            'company_email': settings.COMPANY_EMAIL,
-        }
         subject = _('%s backup report "%s"') % (
             settings.COMPANY_NAME, hostgroup.name)
         if hosts_disabled or hosts_failed:
             subject += _(' (%d failed, %d disabled)') % (
                 hosts_failed, hosts_disabled)
 
-        message = render_to_string('planb/report_email_body.txt', context)
-        try:
-            html_message = check_output(['rst2html'], input=(
-                message.encode('utf-8')))
-        except OSError:
-            html_message = None
-        else:
-            html_message = html_message.decode('utf-8')
+        return subject
 
-        for recipient in (hostgroup.notify_email or [settings.COMPANY_EMAIL]):
-            recipient = recipient.strip()
-            if not recipient:
-                continue
-            self.stdout.write(
-                'Sending report for {} to {}'.format(hostgroup, recipient))
-            self.send_mail(
-                subject=subject,
-                text_message=message,
-                html_message=html_message,
-                from_email=settings.DEFAULT_FROM_EMAIL,
-                recipient_list=[recipient],
-                bcc_list=[settings.COMPANY_EMAIL])
+    def generate_text(self, hostgroup, hosts):
+        context = {
+            'hostgroup': hostgroup,
+            'hosts': hosts,
+            'company_name': settings.COMPANY_NAME,
+            'company_email': settings.COMPANY_EMAIL,
+        }
+
+        return render_to_string('planb/report_email_body.txt', context)
+
+    def generate_html(self, text):
+        try:
+            # Run rst2html binary and hope that it exists.
+            html = check_output(['rst2html'], input=text.encode('utf-8'))
+        except OSError:
+            html = None
+        else:
+            html = html.decode('utf-8')
+
+        return html
+
+    def output_text(self, hostgroup, hosts, force_send):
+        text = self.generate_text(hostgroup, hosts)
+        self.stdout.write(text)
+
+    def output_html(self, hostgroup, hosts, force_send):
+        text = self.generate_text(hostgroup, hosts)
+        html = self.generate_html(text)
+        self.stdout.write(html)
+
+    def output_email(self, hostgroup, hosts, force_send):
+        if not hostgroup.notify_email:
+            self.stderr.write(
+                'No notify addresses for group {}, skipping..'.format(
+                    hostgroup))
+            return
+
+        if not force_send and hostgroup.last_monthly_report:
+            a_while_ago = timezone.now() - timedelta(days=25)
+            if hostgroup.last_monthly_report > a_while_ago:
+                self.stderr.write(
+                    'Already sent to group {} recently, skipping..'.format(
+                        hostgroup))
+                return
+
+        subject = self.generate_subject(hostgroup, hosts)
+        text = self.generate_text(hostgroup, hosts)
+        html = self.generate_html(text)
+
+        recipients = hostgroup.notify_email
+        assert recipients and all('@' in i for i in recipients), recipients
+        self.stdout.write(
+            'Sending report for {} to {}'.format(hostgroup, recipients))
+        self.send_mail(
+            subject=subject, text_message=text, html_message=html,
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=recipients,
+            bcc_list=[settings.COMPANY_EMAIL])
+
         hostgroup.last_monthly_report = timezone.now()
         hostgroup.save(update_fields=['last_monthly_report'])
 

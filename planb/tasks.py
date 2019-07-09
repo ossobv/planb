32936fd7bbad5ba189d91e2fcfa8e33976eaef53
Harm Geerts <hgeerts@osso.nl>
2019-07-09 16:46:25 +0200

    Add dutree to planb.tasks

--- planb/tasks.py
+++ planb/tasks.py
@@ -1,3 +1,4 @@
-from planb.core.tasks import conditional_run, manual_run, spawn_backup_jobs
+from planb.core.tasks import (
+    conditional_run, dutree_run, manual_run, spawn_backup_jobs)
 
-__all__ = ('conditional_run', 'manual_run', 'spawn_backup_jobs')
+__all__ = ('conditional_run', 'dutree_run', 'manual_run', 'spawn_backup_jobs')
